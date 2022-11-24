fed01d54d6853eb5b618389c7c89c65a535d16f6
Himanshu Kumar <avishekgop5833@gmail.com>
2022-11-24 23:29:54 +0530

    added implementation

--- diff.java
+++ diff.java
@@ -6,9 +6,9 @@ public class diff {
    public static void main(String[] args) {
     int a = 2;
     int b = 4;
-    int sum;
-    sum = a+b;
-    System.out.println(sum);
+    int diff;
+    diff = a-b;
+    System.out.println(diff);
    }
         
     }
